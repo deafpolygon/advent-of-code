af993b2ee7c93901fbe7012f9e83533c80695eb7
deafpolygon <deafpolygon@gmail.com>
2022-12-03 12:02:08 +0100

    day 3 solution

--- 2/day2.cpp
+++ 2/day2.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
 #include <list>
-#include <string>
 #include <sstream>
+#include <string>
 #include <tuple>
+#include <vector>
 
 using namespace std;
-vector<tuple<string,string>> process_input();
-void part1(const vector<tuple<string,string>> &data);
-void part2(const vector<tuple<string,string>> &data);
+vector<tuple<string, string>> process_input();
+void part1(const vector<tuple<string, string>>& data);
+void part2(const vector<tuple<string, string>>& data);
 
 // main func
 int main() {
@@ -20,7 +20,7 @@ int main() {
     return 0;
 }
 
-vector<tuple<string,string>> process_input() {
+vector<tuple<string, string>> process_input() {
     ifstream inputfile;
     inputfile.open("input", ios::in);
 
@@ -29,48 +29,60 @@ vector<tuple<string,string>> process_input() {
         exit(1);
     }
 
-    tuple<string,string> play;
-    vector<tuple<string,string>> data;
+    tuple<string, string> play;
+    vector<tuple<string, string>> data;
 
     string line;
-    while(getline(inputfile,line)) {
+    while (getline(inputfile, line)) {
         string opponent, play;
         stringstream s(line);
         s >> opponent >> play;
-        data.push_back(make_tuple(opponent,play));
+        data.push_back(make_tuple(opponent, play));
     }
 
     return data;
-
 }
 
-void part1(const vector<tuple<string,string>> &data) {
+int result(const string& opponent, const string& play) {
+    vector<string> prs = {"A", "B", "C"};
+    auto* p1 = find(prs.front(), prs.back(), opponent);
+    rotate(prs.begin(), prs.begin() - p1, prs.end());
+
+    if (play == prs[1])
+        return 0;
+    else if (play == prs[2])
+        return 6;
+    else
+        return 3;
+}
+
+void part1(const vector<tuple<string, string>>& data) {
     // A, X = rock - 1pt
     // B, Y = paper - 2pt
     // C, Z = scissor - 3pt
     // loss = 0pt, draw = 3pt, win = 6pt
     int score = 0;
+
+    int* var;
     for (auto [opponent, play] : data) {
-        //cout << "Opponent: " << opponent << " You: " << play << endl;
-        if (play == "X") { 
-            score += 1; 
+        // cout << "Opponent: " << opponent << " You: " << play << endl;
+        if (play == "X") {
+            score += 1;
             if (opponent == "A")
                 score += 3;
             else if (opponent == "B")
                 score += 0;
             else if (opponent == "C")
                 score += 6;
-        }
-        else if (play == "Y") { 
-            score += 2; 
+        } else if (play == "Y") {
+            score += 2;
             if (opponent == "A")
                 score += 6;
             else if (opponent == "B")
                 score += 3;
             else if (opponent == "C")
                 score += 0;
-        }
-        else if (play == "Z") {
+        } else if (play == "Z") {
             score += 3;
             if (opponent == "A")
                 score += 0;
@@ -83,44 +95,42 @@ void part1(const vector<tuple<string,string>> &data) {
     cout << "part 1 score: " << score << endl;
 }
 
-void part2(const vector<tuple<string,string>> &data) {
+void part2(const vector<tuple<string, string>>& data) {
     // A, X = rock - 1pt
     // B, Y = paper - 2pt
     // C, Z = scissor - 3pt
     // loss = 0pt, draw = 3pt, win = 6pt
     int score = 0;
     for (auto [opponent, play] : data) {
-        //cout << "Opponent: " << opponent << " You: " << play << endl;
-        if (play == "X") { // lose
-            score += 0; 
-            if (opponent == "A") // rock - scissor
-                score += 3;
-            else if (opponent == "B") // paper - rock
-                score += 1;
-            else if (opponent == "C") // scissor - paper
-                score += 2;
-        }
-        else if (play == "Y") { // draw
-            score += 3; 
-            if (opponent == "A") // rock - rock
-                score += 1;
-            else if (opponent == "B") // paper - paper 
-                score += 2;
-            else if (opponent == "C") // scissor - scissor
-                score += 3;
-        }
-        else if (play == "Z") { // win
-            score += 6;
-            if (opponent == "A") // rock - paper
-                score += 2;
-            else if (opponent == "B") // paper - scissor
-                score += 3;
-            else if (opponent == "C") // scissor - rock
-                score += 1;
-        }
+        // cout << "Opponent: " << opponent << " You: " << play << endl;
+        if (play == "X") {  // lose
 
-        
+            score += 0;
+            if (opponent == "A") {  // rock - scissor
+                score += 3;
+            } else if (opponent == "B") {  // paper - rock
+                score += 1;
+            } else if (opponent == "C") {  // scissor - paper
+                score += 2;
+            }
+
+        } else if (play == "Y") {  // draw
+            score += 3;
+            if (opponent == "A")  // rock - rock
+                score += 1;
+            else if (opponent == "B")  // paper - paper
+                score += 2;
+            else if (opponent == "C")  // scissor - scissor
+                score += 3;
+        } else if (play == "Z") {  // win
+            score += 6;
+            if (opponent == "A")  // rock - paper
+                score += 2;
+            else if (opponent == "B")  // paper - scissor
+                score += 3;
+            else if (opponent == "C")  // scissor - rock
+                score += 1;
+        }
     }
     cout << "part 2 score: " << score << endl;
 }
-
