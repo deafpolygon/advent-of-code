bd206ee5c714dfba20aea85df6ba71ad7ac61f2b
Adam <papashou@gmail.com>
2023-12-01 14:21:53 +0100

    tidying
    cleaning up day1 solution.

--- 2023/day01/index.js
+++ 2023/day01/index.js
@@ -1,8 +1,7 @@
-// setup
-
 const fs = require('fs');
 const path = require('path');
 const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
+const validNumberString = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
 partOne();
 partTwo();
@@ -12,14 +11,11 @@ function getNumberPair(value) {
     return Number(numOnly.slice(0,1) + numOnly.slice(-1));
 }
 
-function filterStringForPuzzle(value) {
-    const validNumberString = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+function filterStringForPuzzle(value) {    
     let processing = value;
-
     //bit of a cheat, but it works for this puzzle
     validNumberString.forEach(function(validnum, idx) {
-        let numval = idx + 1;
-        const replacement = `${validnum.slice(0,1)}${numval}${validnum.slice(-1)}`;
+        const replacement = `${validnum.slice(0,1)}${idx+1}${validnum.slice(-1)}`;
         processing = processing.replaceAll(validnum, replacement);
     });
     return processing;
@@ -32,7 +28,6 @@ function add(accumulator, a) {
 function partOne() {
     const inputarr = input.split('\n');
     console.log(inputarr.map(getNumberPair).reduce(add));
-    
 }
 
 function partTwo() {
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Working on the 2023 puzzles this year. 
 
+Note: All input files (input.txt, input-test.txt) are not committed to this. 
+
